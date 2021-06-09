c2c921388af8026c80bcbcce6962099fbffcc8de
winter-apple <17857524468@163.com>
2021-06-09 15:03:08 +0800

    '完成登录功能'

--- src/main.js
+++ src/main.js
@@ -2,9 +2,18 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 
+import 'element-ui/lib/theme-chalk/index.css'
+import './element/index'
+
+import './assets/css/global.css'
+import axios from 'axios'
+// 配置请求的根路径
+axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1/'
+Vue.prototype.$http = axios
+
 Vue.config.productionTip = false
 
 new Vue({
-  router,
-  render: h => h(App)
+	router,
+	render: (h) => h(App),
 }).$mount('#app')
