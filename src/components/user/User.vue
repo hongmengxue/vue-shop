aa05a10885ac3c66b6f87d0dcac949e3ef2dbafa
winter-apple <17857524468@163.com>
2021-06-13 11:01:05 +0800

    '完成用户列表的开发'

--- src/components/user/User.vue
+++ src/components/user/User.vue
@@ -359,7 +359,7 @@ export default {
       if (res.meta.status !== 200) {
         return this.$message.error('删除用户失败')
       }
-      this.$message.error('删除用户成功')
+      this.$message.success('删除用户成功')
       this.getUserList()
     },
   },
