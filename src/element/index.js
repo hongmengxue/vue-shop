386fd019f09f8d3c8f807107a8eb4da48c995fb6
winter-apple <17857524468@163.com>
2021-06-17 18:00:25 +0800

    完成了权限功能的开发

--- src/element/index.js
+++ src/element/index.js
@@ -24,14 +24,22 @@ import {
   Tooltip,
   Pagination,
   Dialog,
-  MessageBox
+  MessageBox,
+  Tag,
+  Tree,
+  Select,
+  Option
 } from 'element-ui'
 
 Vue.use(Button)
 Vue.use(Tooltip)
 Vue.use(Pagination)
 Vue.use(Dialog)
-Vue.use(Form)
+Vue.use(Tag)
+Vue.use(Tree)
+Vue.use(Select)
+Vue.use(Tree)
+Vue.use(Option)
 Vue.use(FormItem)
 Vue.use(Input)
 Vue.use(Container)
